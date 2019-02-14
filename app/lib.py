fb61d0714785bb5f7b087c3975ec93e7fec81134
MezentsevTA <timur-mezentsev@yandex.ru>
2019-02-14 15:56:17 +0300

    tests written

--- app/lib.py
+++ app/lib.py
@@ -1,4 +1,14 @@
 def bonus_count(buy_sum, acc_sum):
+    """
+    >>> bonus_count(0, 0)
+    0
+    >>> bonus_count(1100, 900)
+    100
+    >>> bonus_count(10_000, 5_001)
+    700
+    >>> bonus_count(100_000, 5_0001)
+    10_000
+    """
     if buy_sum + acc_sum < 15_001:
         bonus = (buy_sum % 1000 + acc_sum)// 1000 * 50
     if 15_000 <= buy_sum + acc_sum < 150_001:
@@ -7,4 +17,4 @@ def bonus_count(buy_sum, acc_sum):
         bonus = (buy_sum % 1000 + acc_sum) // 1000 * 100
     return bonus
 
-print(bonus_count(1970, 1230))
+
